46ad33cb77647a3dc743985dff577aa3067f1745
Iratethisname10 <115908538+Iratethisname10@users.noreply.github.com>
2022-12-24 10:00:17 +0800

    Create dad.lua

---
+++ dad.lua
@@ -0,0 +1,7 @@
+local keydata = loadstring(game:HttpGet("https://raw.githubusercontent.com/Iratethisname10/test/main/01"))()
+
+for i,v in pairs(keydata) do
+    if v == getgenv().MainKey then
+        print("uwu")
+    end
+end
